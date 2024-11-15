94167815051293da11c9a017bb0a11971e9abf96
Zaidbhati10114 <zaidbhati007@gmail.com>
2024-11-15 19:27:52 +0530

    fixed issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,8 @@ const eb_garamond = EB_Garamond({
 })
 
 export const metadata: Metadata = {
-  title: "jStack App",
-  description: "Created using jStack",
+  title: "Watch Tower",
+  description: "Notifications Service SASS",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 }
 
